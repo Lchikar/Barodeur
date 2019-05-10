1093f768034c3f20c1e3c62c7888e3a362827a94
Unknown <lucie.lesbats@hotmail.fr>
2019-05-10 10:38:03 +0200

    Essais connexion bdd

--- front/connexion_compt.php
+++ front/connexion_compt.php
@@ -1,25 +1,26 @@
- <?php 
-  session_start(); // on ouvre une session pour utiliser les cookies
-  session_unset (); // met à zero les cookies au cas où il y avait une autre session d'ouverte
-  include("connexion.php"); // connexion à la bdd
-  $res = $bdd->query("SELECT pseudo, mdp FROM User;"); // recherche des tous les users
+<?php 
+session_start(); // on ouvre une session pour utiliser les cookies
+session_unset (); // met à zero les cookies au cas où il y avait une autre session d'ouverte
+include("connexion.php"); // connexion à la bdd
+$res = $bdd->query("SELECT pseudo, mdp FROM User;"); // recherche des tous les users
 
-   if (isset($_POST['pseudo']) && isset($_POST['mdp'])) { // vérification des variables du formulaire
-
+if (isset($_POST['pseudo']) && isset($_POST['mdp'])) { // vérification des variables du formulaire
     while($ligne = $res->fetch() ){ // parcours de la requete (liste des pseudos et mdp de chaque user)
-      if( $_POST['pseudo'] == $ligne['pseudo'] && sha1($_POST['mdp']) == $ligne['mdp']){
+        if( $_POST['pseudo'] == $ligne['pseudo'] && sha1($_POST['mdp']) == $ligne['mdp']){
         // user retrouvé
         $_SESSION['pseudo'] = $_POST['pseudo'];
         $_SESSION['mdp'] = $_POST['mdp'];
-      }			
+        }
     }
     $res->closeCursor();
     if (isset($_SESSION['pseudo'])){
-      header('location: page_principale.html'); // connexion reussie, redirection vers profil user
+        header('location: page_principale.html'); // connexion reussie, redirection vers profil user
+        exit();
     }
     else{
-      header('location: accueil.html'); // echec connexion, redirection page de connexion
+        header('location: accueil.html'); // echec connexion, redirection page de connexion
+        exit();
     }
-   }
+}
 
-   ?>
+?>
--- front/js/redirection.js
+++ front/js/redirection.js
@@ -1,21 +1,49 @@
-function Connexion(){
-	connect = document.getElementById('submitConnexion');
-	form= document.getElementById('formConnexion');
-	
-	
-	if(connect.value="Connexion"){
-		form.action="connexion_compt.php";
-	}
-	
-}
+document.addEventListener("DOMContentLoaded", function (event) {
+   const btnConnexion = document.getElementById("submitConnexion");
+    
+    btnConnexion.addEventListener("click", seConnecter);
+    
+    
+    /*btnConnexion.onclick = () => {
+        console.log("hey");
+        const form = document.getElementById('formConnexion');
 
-function Inscription(){
-	connect = document.getElementById('submitConnexion');
-	form= document.getElementById('formConnexion');
-	
-	
-	if(connect.value="Inscription"){
-		form.action="inscription.php";
-	}
-	
-}
+        form.action = "connexion_compt.php";
+        
+    }*/
+    //console.log(btnConnexion.onclick);
+    
+    
+    /*const btnInscription = document.getElementById("submitInscription");
+    btnConnexion.onclick = event => {
+        event.preventDefault();
+        const form = document.getElementById('formConnexion');
+
+        form.action = "inscription.php";
+        
+    }*/
+    
+    
+    function seConnecter(evt){
+        console.log("hello");
+        const form = document.getElementById('formConnexion');
+
+        form.action = "connexion_compt.php";
+        console.log("j'ai cliqué");
+    }
+
+
+    /*function connexion(evt) {
+        const form = document.getElementById('formConnexion');
+        form.action = "connexion_compt.php";
+        
+
+    }
+
+    function inscription(evt) {
+        const form = document.getElementById('formConnexion');
+        form.action = "inscription.php";
+        
+
+    }*/
+});
