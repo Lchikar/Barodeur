cfa0ebe39d080045c7bb731cdd64c90159afcd54
Unknown <lucie.lesbats@hotmail.fr>
2019-05-10 10:38:15 +0200

    essais connexion bdd

--- front/connexion_compt.php
+++ front/connexion_compt.php
@@ -2,7 +2,7 @@
 session_start(); // on ouvre une session pour utiliser les cookies
 session_unset (); // met à zero les cookies au cas où il y avait une autre session d'ouverte
 include("connexion.php"); // connexion à la bdd
-$res = $bdd->query("SELECT pseudo, mdp FROM User;"); // recherche des tous les users
+$res = $pdo->query("SELECT pseudo, mdp FROM User;"); // recherche des tous les users
 
 if (isset($_POST['pseudo']) && isset($_POST['mdp'])) { // vérification des variables du formulaire
     while($ligne = $res->fetch() ){ // parcours de la requete (liste des pseudos et mdp de chaque user)
