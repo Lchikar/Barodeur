cdd283cdaa57a473f1a57d50eecb8264aeb5e2a7
Louisa Chikar <louisa.chikar@gmail.com>
2019-05-12 18:52:22 +0200

    Connexion OK muthafukaaaa

--- front/connexion_compt.php
+++ front/connexion_compt.php
@@ -2,23 +2,31 @@
 session_start(); // on ouvre une session pour utiliser les cookies
 session_unset (); // met à zero les cookies au cas où il y avait une autre session d'ouverte
 require_once 'MyPDO.db.include.php'; // connexion à la bdd
-$res = $pdo->query("SELECT pseudo, mdp FROM User;"); // recherche des tous les users
+$stmt = MyPDO::getInstance()->prepare(<<<SQL
+    SELECT pseudo, password FROM User
+SQL
+); 
+// recherche des tous les users
+
+$stmt->execute();
+
 
 if (isset($_POST['pseudo']) && isset($_POST['mdp'])) { // vérification des variables du formulaire
-    while($ligne = $res->fetch() ){ // parcours de la requete (liste des pseudos et mdp de chaque user)
+    while(($ligne = $stmt->fetch())){ // parcours de la requete (liste des pseudos et mdp de chaque user)
         if( $_POST['pseudo'] == $ligne['pseudo'] && sha1($_POST['mdp']) == $ligne['mdp']){
         // user retrouvé
         $_SESSION['pseudo'] = $_POST['pseudo'];
         $_SESSION['mdp'] = $_POST['mdp'];
+        $connexion = 0;
         }
     }
-    $res->closeCursor();
-    if (isset($_SESSION['pseudo'])){
+
+    if ($connexion==0){
         header('location: page_principale.html'); // connexion reussie, redirection vers profil user
         exit();
     }
     else{
-        header('location: accueil.html'); // echec connexion, redirection page de connexion
+        header('location: accueil.html?err=err'); // echec connexion, redirection page de connexion
         exit();
     }
 }
