5f60ac19dc509939c4c31f22b6fa5f8d085fa47f
Louisa Chikar <louisa.chikar@gmail.com>
2019-05-26 15:39:45 +0200

    recherche bar like

--- FINAL/php/recherche_bar.php
+++ FINAL/php/recherche_bar.php
@@ -42,50 +42,54 @@ require_once '../MyPDO_config/MyPDO.db.include.php'; // connexion à la bdd
 
     <div id="AllBars">
         <?php
-				$stmt =  MyPDO::getInstance()->prepare(
-				"SELECT name, photo, 
-				CONCAT (numStreet, ' ', street, ' ', postalCode,' ', cityName) as adresse, BarType.barType as type
-				FROM Bar NATURAL JOIN City NATURAL JOIN BarBelongsType NATURAL JOIN BarType
-				WHERE Bar.name = :recherche OR cityName= :recherche OR postalCode= :recherche OR barType= :recherche");
-				$stmt->bindValue(':recherche', $_GET['rechercher']);
-				$stmt->execute();	
-				while($general = $stmt->fetch()){
-					echo '<div class="classer"><a href="afficher_bar.php?bar='.$general["name"].'" style="text-decoration: none">';
-					echo '<div id="affiche_bar" onClick="ChangePage()">';
-							$src = "../image/bars/".$general['photo'];
-							echo ('<div id="picture"> <img class="photo"
-							 src="'.$src.'"
-							 alt='.$general['name'].'/>');
-							 echo '</div>';
-							 
-							 echo('<br><div id="infos">');
-							 echo "<br>".$general['name']."<br>";
-							 echo "<br>".$general['adresse']."<br>";
-							 echo "<br>Bar à ".$general['type']."<br>";
-							 echo '</div>';
-							 
-							 echo '<div id="moy">';
-							 $stmt2 =  MyPDO::getInstance()->prepare(
-								"SELECT Mark.value as 'value'
-								FROM Bar NATURAL JOIN Mark NATURAL JOIN MarkType 
-								WHERE Bar.name =:bar AND MarkType.markType = 'general'");
-								$stmt2->bindValue(':bar', $general['name']); 
-								
-								$stmt2->execute();
-								$cpt = 0; $somme = 0;	
-								while($note = $stmt2->fetch()){
-									$cpt ++;
-									$somme += $note['value'];
-								}
-								if(0 == $cpt) $moy = 0;
-								else $moy = $somme/$cpt;
-								echo "<br>Note générale: ".$moy."/5";
-								echo "</div>";
-							echo "</div>";
-							echo '</a></div>';
-							echo '<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
+			$stmt =  MyPDO::getInstance()->prepare(
+			"SELECT name, photo, 
+			CONCAT (numStreet, ' ', street, ' ', postalCode,' ', cityName) as adresse, BarType.barType as type
+			FROM Bar NATURAL JOIN City NATURAL JOIN BarBelongsType NATURAL JOIN BarType
+			WHERE Bar.name LIKE :recherche OR cityName LIKE :recherche OR postalCode LIKE :recherche OR barType LIKE :recherche");
+			$stmt->bindValue(':recherche', '%'.$_GET['rechercher'].'%');
+			$stmt->execute();
+             $cpt_res = 0;	
+			while($general = $stmt->fetch()){
+                $cpt_res++;
+				echo '<div class="classer"><a href="afficher_bar.php?bar='.$general["name"].'" style="text-decoration: none">';
+				echo '<div id="affiche_bar" onClick="ChangePage()">';
+					$src = "../image/bars/".$general['photo'];
+					echo ('<div id="picture"> <img class="photo"
+					 src="'.$src.'"
+					 alt='.$general['name'].'/>');
+					 echo '</div>';
+					 
+					 echo('<br><div id="infos">');
+					 echo "<br>".$general['name']."<br>";
+					 echo "<br>".$general['adresse']."<br>";
+					 echo "<br>Bar à ".$general['type']."<br>";
+					 echo '</div>';
+					 
+					 echo '<div id="moy">';
+					 $stmt2 =  MyPDO::getInstance()->prepare(
+						"SELECT Mark.value as 'value'
+						FROM Bar NATURAL JOIN Mark NATURAL JOIN MarkType 
+						WHERE Bar.name =:bar AND MarkType.markType = 'general'");
+						$stmt2->bindValue(':bar', $general['name']); 
+						
+						$stmt2->execute();
+						$cpt = 0; $somme = 0;	
+						while($note = $stmt2->fetch()){
+							$cpt ++;
+							$somme += $note['value'];
 						}
-					?>
+						if(0 == $cpt) $moy = 0;
+						else $moy = $somme/$cpt;
+						echo "<br>Note générale: ".$moy."/5";
+						echo "</div>";
+					echo "</div>";
+					echo '</a></div>';
+					echo '<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
+				}
+                if(0 == $cpt_res)
+                    echo "Aucun resultat pour \"".$_GET['rechercher']."\"<br>";
+		?>
     </div>
 
     <!--deuxieme interface quand on clique sur le bouton-->
